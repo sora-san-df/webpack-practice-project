cbec77349fb31ece9d7bee73c3ab6f31a3c7fd82
cloud <cloudintheskyoutlook.es>
2022-10-24 19:21:45 -0500

    [ADD]Netlify

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin'); 
+const cssMinizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+
+module.exports = {
+    entry: './src/index.js',
+    output:{
+        path: path.resolve(__dirname,'dist'),
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]',
+        clean: true 
+    },
+    resolve:{
+        extensions: ['.js'],
+        alias: {
+            '@utils': path.resolve(__dirname, 'src/utils/'),
+            '@templates': path.resolve(__dirname, 'src/templates/'),
+            '@styles': path.resolve(__dirname, 'src/styles/'),
+            '@images': path.resolve(__dirname, 'src/assets/images/'),
+        }
+    },
+    module:{
+        rules:[
+        {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        },
+        {
+            test: /\.css|.styl$/i,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'stylus-loader'
+            ]
+        },
+        {
+        test: /\.png/,
+        type: "asset/resource"
+        },
+        {
+        test: /\.(woff|woff2)$/,
+        type: 'asset/resource',
+        generator: {
+            filename: 'static/fonts/[hash][ext][query]',
+        }
+        }
+    ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'asset/[name].[contenthash].css'
+        }),
+        new CopyPlugin({
+            patterns: [{
+                from: path.resolve(__dirname, "src","assets/images"),
+                to: "assets/images"
+            }]
+        }),
+        new Dotenv(),
+    ],
+    optimization:{
+        minimize: true,
+        minimizer:[
+            new cssMinizerPlugin(),
+            new TerserPlugin() 
+        ]
+    }
+}
